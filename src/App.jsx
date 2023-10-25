4d07c12f134cc68ba328386687ccb485756508f4
Tero Leppänen <tero.leppanen@live.fi>
2023-10-25 22:22:47 +0300

    Setup Styled Components. Make a few components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => console.log("check in")}>Check in</Button>
+        <Button onClick={() => console.log("check out")}>Check in</Button>
+        <Input type="number" placeholder="Number of quests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
