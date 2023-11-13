c7d6227666467e7e6b00e39559c6131caa0d6368
Tero Leppänen <tero.leppanen@live.fi>
2023-11-13 17:49:27 +0200

    Make hook to listen outside clicks which closes modal.

---
+++ src/hooks/useOutsideClick.js
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from "react";
+
+export function useOutsideClick(handler, listenCapturing = true) {
+  const ref = useRef();
+
+  useEffect(
+    function () {
+      function handleClick(e) {
+        if (ref.current && !ref.current.contains(e.target)) {
+          handler();
+        }
+      }
+
+      // true makes event to be handled in capturing phase, ie. when event is going down
+      document.addEventListener("click", handleClick, listenCapturing);
+
+      return () =>
+        document.removeEventListener("click", handleClick, listenCapturing);
+    },
+    [handler, listenCapturing]
+  );
+
+  return ref;
+}
+
+export default useOutsideClick;
