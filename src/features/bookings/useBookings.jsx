bf7d29a744a4653e0c571361e9f1b952eb266366
Tero Leppänen <tero.leppanen@live.fi>
2023-11-22 11:31:44 +0200

    Add bookings filtering using supabase queries.

--- src/features/bookings/useBookings.jsx
+++ src/features/bookings/useBookings.jsx
@@ -1,13 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // 1. Filter
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+  // { field: "totalPrice", value: 5000, method: "gte" };
+
   const {
     data: bookings,
     isLoading,
     error,
-  } = useQuery({ queryKey: ["bookings"], queryFn: getBookings });
+  } = useQuery({
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
+  });
 
   return { bookings, isLoading, error };
 }
