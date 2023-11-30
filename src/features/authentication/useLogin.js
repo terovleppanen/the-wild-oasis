53f2f274045b3bfa0903e9606411a28878313ef4
Tero Leppänen <tero.leppanen@live.fi>
2023-11-30 17:57:20 +0200

    Add use login feature.

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,24 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+import { login as loginAPI } from "../../services/apiAuth";
+
+export function useLogin() {
+  const navigate = useNavigate();
+
+  const { mutate: login, isLoading } = useMutation({
+    mutationFn: ({ email, password }) => loginAPI({ email, password }),
+
+    onSuccess: (user) => {
+      navigate("/dashboard");
+    },
+
+    onError: (error) => {
+      console.log("ERROR:", error);
+      toast.error("Provided email or password are incorrect");
+    },
+  });
+
+  return { login, isLoading };
+}
