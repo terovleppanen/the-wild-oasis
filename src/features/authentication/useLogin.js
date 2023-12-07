13792339c1269e3a8b85a5dcb781ace6dbbe4d42
Tero Leppänen <tero.leppanen@live.fi>
2023-12-07 18:12:30 +0200

    Add logout functionality.

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -14,7 +14,7 @@ export function useLogin() {
 
     onSuccess: (user) => {
       queryClient.setQueriesData(["user"], user);
-      navigate("/dashboard");
+      navigate("/dashboard", { replace: true });
     },
 
     onError: (error) => {
