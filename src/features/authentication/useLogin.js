4da598c53118339fa296d6b911ef75cb7e7e0591
Tero Leppänen <tero.leppanen@live.fi>
2023-12-07 17:55:55 +0200

    Implement functionality for protected routes.

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,16 +1,19 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
 import { login as loginAPI } from "../../services/apiAuth";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
+
   const navigate = useNavigate();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginAPI({ email, password }),
 
     onSuccess: (user) => {
+      queryClient.setQueriesData(["user"], user);
       navigate("/dashboard");
     },
 
---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import Spinner from "./Spinner";
+import { useUser } from "../features/authentication/useUser";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
+  // 1. Load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  // 3. If there is no authenticated user, redirect to the /login
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [navigate, isAuthenticated, isLoading]
+  );
+
+  // 2. While loading, show a spinner
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  // 4. If there is a user, render the app
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
